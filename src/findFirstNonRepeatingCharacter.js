0fdb457e00e61911117868f57d384ea85ee6c9f4
jay.isidro <jaenelleisidro@gmail.com>
2023-02-12 08:18:21 +0800

    using findIndex

--- src/findFirstNonRepeatingCharacter.js
+++ src/findFirstNonRepeatingCharacter.js
@@ -1,6 +1,14 @@
 module.exports = (str)=>{
 
-    //add logic here
-    
-    return null;
+    if(typeof str!='string'){throw Error("parameter must be a string")}
+        
+    let chars=str.split("");
+
+    return chars.find((char,i)=>{
+        let index=chars.findIndex((c,i2)=>{
+            return c==char && i!=i2;
+        });
+        
+        if(index==-1){return char}
+    })||null;
 }
