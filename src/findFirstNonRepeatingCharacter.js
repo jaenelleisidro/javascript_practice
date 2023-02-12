f070a4885c91e88e1ad6c8f50c6b3f72aa9f8aed
jay.isidro <jaenelleisidro@gmail.com>
2023-02-12 08:16:43 +0800

    using every

--- src/findFirstNonRepeatingCharacter.js
+++ src/findFirstNonRepeatingCharacter.js
@@ -1,6 +1,16 @@
 module.exports = (str)=>{
-
-    //add logic here
+    if(typeof str!='string'){throw Error("parameter must be a string")}
     
+    let chars=str.split("");
+    for (let i = 0; i < chars.length; i++) {
+        const char = chars[i];
+        let isNonRepeating=chars.every((c,i2)=>{
+            return c!=char || i==i2;
+        });
+
+        
+        if(isNonRepeating){return char}
+    }
+
     return null;
 }
